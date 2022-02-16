bbf4af955bf5223059f379d0d0454f8542428093
Richard <au.richard4@gmail.com>
2022-02-15 19:29:47 -0800

    Timer Function to test beeps at given input

---
+++ timer1.js
@@ -0,0 +1,27 @@
+const args = process.argv.slice(2);
+const timer = function (args) {
+  console.log(args);
+  if (args.length === 0) {
+    return "";
+  }
+  for (let i = 0; i < args.length; i++) {
+    if (isNaN(args[i])) {
+      // console.log("Not a valid input");
+      continue;
+    }
+    if ((!isNaN(args[i])) && (args[i] < 0)) {
+      // console.log("Not a valid input");
+      continue;
+    }
+    if ((!isNaN(args[i])) && (args[i] > 0)) {
+      setTimeout(() => {
+        process.stdout.write('\x07');
+
+        // console.log("BEEP!");
+      }, args[i] * 1000);
+    }
+  }
+};
+
+console.log(timer(args));
+
